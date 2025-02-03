06f2c18c654102a84b40501fca51cf0da029f812
root <root@oel03.linoop.com>
2025-02-03 14:28:10 -0500

    added new files

--- script1.sh
+++ script1.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 echo "This is from another script"
 hostname
+=======
+echo -e "\v\v======================= PRINTING MACHINE NAME ========================\v\v"
+hostnamcectl 
+echo -e "\v\v======================= PRINTING DISK USAGE ========================\v\v"
+>>>>>>> 9b4e4ff (disk and hostname added)
 df -h 
+echo -e "\v\v"
